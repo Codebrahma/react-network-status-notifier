ebc29f7e159cc3f7041b7ce08fbe252840f731fb
Akhand Mishra <akkimysite@gmail.com>
2019-08-12 12:48:14 +0530

    Indentation fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,29 @@
-var path = require('path');
+var path = require("path");
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'index.js',
-    libraryTarget: 'commonjs2'
+    path: path.resolve(__dirname, "build"),
+    filename: "index.js",
+    libraryTarget: "commonjs2"
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
+        include: path.resolve(__dirname, "src"),
         exclude: /(node_modules|build)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader"
         }
       },
       {
         test: /\.css$/,
-        include: [
-          path.resolve(__dirname, 'src'),
-        ],
+        include: [path.resolve(__dirname, "src")],
         loader: "style-loader!css-loader"
       }
     ]
   },
   externals: {
-    'react': 'commonjs react' 
+    react: "commonjs react"
   }
 };
