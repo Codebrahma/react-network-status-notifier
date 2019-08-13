37c0776bf1dea6e78cd09158db44009133a091c8
Akhand Mishra <akkimysite@gmail.com>
2019-08-13 12:42:45 +0530

    Linting and prettifying

--- src/components/message.test.js
+++ src/components/message.test.js
@@ -1,28 +1,33 @@
-import React from 'react'
+import React from "react";
 
-import { configure, shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-import Message from './message'
+import Message from "./message";
 
-configure({adapter: new Adapter()})
+configure({ adapter: new Adapter() });
 
-describe('<Message />', () => {
-    let wrapper
-    beforeEach(() => {
-        wrapper = shallow(<Message style={{ fontFamily: 'cursive' }} className="myMessage" message="Test message" />)
-    })
-    
-    it('should have a class of HideNSeek-message', () => {
-        expect(wrapper.hasClass('myMessage')).toBeTruthy();
-    })
-    
-    it('should have the message passed to it as a prop', () => {
-        expect(wrapper.text()).toEqual("Test message");
-    })
-    
-    it('should have the style object passed to it as a prop', () => {
-        expect(wrapper.props().style).toMatchObject({ fontFamily: 'cursive' })
-    })
+describe("<Message />", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(
+      <Message
+        style={{ fontFamily: "cursive" }}
+        className="myMessage"
+        message="Test message"
+      />
+    );
+  });
 
-})
+  it("should have a class of HideNSeek-message", () => {
+    expect(wrapper.hasClass("myMessage")).toBeTruthy();
+  });
+
+  it("should have the message passed to it as a prop", () => {
+    expect(wrapper.text()).toEqual("Test message");
+  });
+
+  it("should have the style object passed to it as a prop", () => {
+    expect(wrapper.props().style).toMatchObject({ fontFamily: "cursive" });
+  });
+});
